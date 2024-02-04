70b353d781509189e267bc6cd5bab0d98bfb3038
vitalyvitmens <otm_@tut.by>
2024-02-04 22:12:14 +0300

    chore(src\components\layout\AppHeader.jsx): меняет state drawer на false, передает в пропсы AddAssetForm onClose

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -18,7 +18,7 @@ export default function AppHeader() {
   const [select, setSelect] = useState(false)
   const [coin, setCoin] = useState(null)
   const [modal, setModal] = useState(false)
-  const [drawer, setDrawer] = useState(true)
+  const [drawer, setDrawer] = useState(false)
   const { crypto } = useCrypto()
 
   useEffect(() => {
@@ -77,7 +77,7 @@ export default function AppHeader() {
         open={drawer}
         destroyOnClose
       >
-        <AddAssetForm />
+        <AddAssetForm onClose={() => setDrawer(false)} />
       </Drawer>
     </Layout.Header>
   )
